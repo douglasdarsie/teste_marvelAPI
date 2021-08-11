0a0699665761449ed9d3623a63dd92bb5222a776
douglasdarsie <55034987+douglasdarsie@users.noreply.github.com>
2021-08-11 19:16:14 -0300

    Updated version

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import marvel
+import keys
 
-marvelClass = marvel.characterData()
-marvelClass.getCharacterData("Iron Man")
+marvelClass = marvel.characterData("Iron Man", keys.public_key, keys.private_key)
+marvelClass.getCharacterEvents("Iron Man")
 
 strTable = "<html>\n\t<body>\n\t\t<table>\n\t\t\t<tr>\n\t\t\t\t<th>Marvel table</th>\n\t\t\t\t<th></th>\n\t\t\t</tr>"
 
-strRW = "\n\t\t\t<tr>\n\t\t\t\t<td>"+"Character Name"+ "\n\t\t\t\t</td>\n\t\t\t\t<td>"+str(marvelClass.charName)+"\n\t\t\t\t</td>\n\t\t\t</tr>"
+strRW = "\n\t\t\t<tr>\n\t\t\t\t<td>"+"Character Name"+ "</td>\n\t\t\t\t<td>"+str(marvelClass.charName)+"</td>\n\t\t\t</tr>"
 strTable = strTable+strRW
-strRW = "\n\t\t\t<tr>\n\t\t\t\t<td>Character Image\n\t\t\t\t</td>\n\t\t\t\t<td><img src=\""+str(marvelClass.charImage)+"\" alt=\"pic\"/></td>\n\t\t\t</tr>"
+strRW = "\n\t\t\t<tr>\n\t\t\t\t<td>Character Image</td>\n\t\t\t\t<td><img src=\""+str(marvelClass.charImage)+"\" alt=\"pic\"/></td>\n\t\t\t</tr>"
 strTable = strTable+strRW
-strRW = "\n\t\t\t<tr>\n\t\t\t\t<td>Description\n\t\t\t\t</td>\n\t\t\t\t<td>"+str(marvelClass.charDesc)+"</td>\n\t\t\t</tr>"
+strRW = "\n\t\t\t<tr>\n\t\t\t\t<td>Description</td>\n\t\t\t\t<td>"+str(marvelClass.charDesc)+"</td>\n\t\t\t</tr>"
 strTable = strTable+strRW
-strRW = "\n\t\t\t<tr>\n\t\t\t\t<td>Description\n\t\t\t\t</td>\n\t\t\t\t<td>"+str(marvelClass.atribText)+"</td>\n\t\t\t</tr>"
+strRW = "\n\t\t\t<tr>\n\t\t\t\t<td>Description</td>\n\t\t\t\t<td>"+str(marvelClass.atribText)+"</td>\n\t\t\t</tr>"
 strTable = strTable+strRW
 strRW = "\n\t\t</table>"
 strTable = strTable+strRW
@@ -20,7 +21,7 @@ strRW ="\n\t\t<table>\n\t\t\t<tr>\n\t\t\t\t<th>Related Characters - "+str(marvel
 strTable = strTable+strRW
 
 for i in range(len(marvelClass.relatedCharsImg)):
-    strRW = "\n\t\t\t<tr>\n\t\t\t\t<td>"+str(marvelClass.relatedChars[i])+"\n\t\t\t\t</td>\n\t\t\t\t<td><img src=\""+str(marvelClass.relatedCharsImg[i])+"\" alt=\"pic\"/></td>\n\t\t\t</tr>"
+    strRW = "\n\t\t\t<tr>\n\t\t\t\t<td>"+str(marvelClass.relatedChars[i])+"</td>\n\t\t\t\t<td><img src=\""+str(marvelClass.relatedCharsImg[i])+"\" alt=\"pic\"/></td>\n\t\t\t</tr>"
     strTable = strTable+strRW
 
 strTable = strTable+"\n\t\t</table>\n\t</body>\n</html>"
--- marvel.py
+++ marvel.py
@@ -1,50 +1,49 @@
 import requests
 from hashlib import md5
 from time import time
-import keys
 
-public_key = keys.public_key
-private_key = keys.private_key
-
-class characterData():
-    def __init__(self):
-        self.publicKey = public_key
-        self.privKey = private_key
+class characterData:
+    def __init__(self, characterName:str , publicKey:str , privateKey:str):
+        self.publicKey = publicKey
+        self.privKey = privateKey
+        self.timestamp = str(time())
+        self.hash = self.getHash(publicKey,privateKey)
         self.basicURL = "https://gateway.marvel.com/"
-        self.charName = ""
-        self.charDesc = ""
-        self.charID = 0
-        self.charImage = ""
-        self.atribText = ""
+        self.basicData = self.getBasicData(characterName)
+        self.charName = self.basicData["data"]["results"][0]["name"]
+        self.charDesc = self.basicData["data"]["results"][0]["description"]
+        self.charID = self.basicData["data"]["results"][0]["id"]
+        self.charImage = self.basicData["data"]["results"][0]["thumbnail"]["path"] + "/portrait_xlarge." + self.basicData["data"]["results"][0]["thumbnail"]["extension"]
+        self.atribText = self.basicData["attributionText"]
         self.charEventListID = []
         self.charEventListTitle = []
         self.relatedChars = []
         self.relatedCharsImg = []
-        
-    def getCharacterData(self,characterName:str):
+    
+    def getHash(self, publicKey:str, privateKey:str):
         m = md5()
-        ts = str(time())
+        ts = self.timestamp
         m.update(bytes(ts,'utf-8'))
         m.update(bytes(self.privKey,'utf-8'))
         m.update(bytes(self.publicKey,'utf-8'))
         hash_key = m.hexdigest()
+        return hash_key
+    
+    def getBasicData(self, characterName):
+        ts = self.timestamp
         endpoint = "&orderBy=name&limit=1"
         url = self.basicURL + "v1/public/characters?name=" + characterName + endpoint
-        payload = {"ts":ts,"apikey":public_key,"hash":hash_key}
+        payload = {"ts":ts,"apikey":self.publicKey,"hash":self.hash}
         rChar = requests.get(url , params=payload)
         dataJson = rChar.json()
+        return dataJson
         
-        self.charName = dataJson["data"]["results"][0]["name"]
-        self.charDesc = dataJson["data"]["results"][0]["description"]
-        self.charID = dataJson["data"]["results"][0]["id"]
-        self.atribText = dataJson["attributionText"]
-        
-        self.charImage = dataJson["data"]["results"][0]["thumbnail"]["path"] + "/portrait_xlarge." + dataJson["data"]["results"][0]["thumbnail"]["extension"]
-        
+    def getCharacterEvents(self,characterName):
+        ts = self.timestamp
         baseUrl = self.basicURL+"v1/public/events?characters="
         endpoint = "&orderBy=name&limit=100&"
         url = baseUrl + str(self.charID) + endpoint
-        payload = {"ts":ts,"apikey":public_key,"hash":hash_key}
+        payload = {"ts":ts,"apikey":self.publicKey,"hash":self.hash}
         rEvents = requests.get(url , params=payload)
         dataJson = rEvents.json()
         charEventListTitle = []
@@ -59,7 +58,7 @@ class characterData():
             baseUrl = self.basicURL+"v1/public/events/"
             endpoint = "/characters?orderBy=name&limit=100&"
             url = baseUrl + str(title) + endpoint
-            payload = {"ts":ts,"apikey":public_key,"hash":hash_key}
+            payload = {"ts":ts,"apikey":self.publicKey,"hash":self.hash}
             r = requests.get(url , params=payload)
             dataJson = r.json()
             for item in dataJson["data"]["results"]:
@@ -67,6 +66,7 @@ class characterData():
         charPartnersList = list(dict.fromkeys(charPartnersList))
         charPartnersList.remove(characterName)
         self.relatedChars = charPartnersList
+        
         print("getting related characters...")
         charPartnersImg = []
         i = 0
@@ -74,7 +74,7 @@ class characterData():
             baseUrl = self.basicURL+"v1/public/characters?name="
             endpoint = "&orderBy=name&limit=1"
             url = baseUrl + charName + endpoint
-            payload = {"ts":ts,"apikey":public_key,"hash":hash_key}
+            payload = {"ts":ts,"apikey":self.publicKey,"hash":self.hash}
             r = requests.get(url , params=payload)
             dataJson = r.json()
             try:
@@ -85,3 +85,4 @@ class characterData():
                 break
             i += 1
         self.relatedCharsImg = charPartnersImg
+        
